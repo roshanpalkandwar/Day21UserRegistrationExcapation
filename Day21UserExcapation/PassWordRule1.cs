846835d2847b0f693daac869b3ba229402b5518a
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-26 22:36:09 +0530

    [Roshan]Valid Passwor RUle1

---
+++ Day21UserExcapation/PassWordRule1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day21UserExcapation
+{
+    internal class PassWordRule1
+    {
+        public static void ValidatePassword()
+        {
+            try
+            {
+                Console.WriteLine("Enter Password");
+                string password = Console.ReadLine();
+                var hasLowerChar = new Regex("^([a-z]){8,}$");
+
+                if (!hasLowerChar.IsMatch(password))
+                {
+                    throw (new CustomException("Password is not valid... Password should have minimum 8 characters"));
+                }
+                else
+                {
+                    Console.WriteLine("Password : " + password);
+                }
+            }
+            catch (CustomException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
