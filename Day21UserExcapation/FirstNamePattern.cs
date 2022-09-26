bc5d4b324d6130835a437e6f02a30ac754e7f82a
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-26 22:10:34 +0530

    [Roshan]uc1-Valid First Name

---
+++ Day21UserExcapation/FirstNamePattern.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day21UserExcapation
+{
+    public class FirstNamePattern
+    {
+        public static void ValidateFirstName()
+        {
+            try
+            {
+                Console.WriteLine("Enter First Name");
+                String firstName = Console.ReadLine();
+                string pattern = "^[A-Z]{1}[a-z]{2,}$";
+                if (Regex.IsMatch(firstName, pattern))
+                {
+                    Console.WriteLine("First Name : " + firstName);
+                }
+                else
+                {
+                    throw (new CustomException("First name should be first letter capital and more then 2 characters"));
+                }
+            }
+            catch (CustomException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ Day21UserExcapation/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Day21UserExcapation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Choose a option");
+            Console.WriteLine("Chosee a oprtion");
+            Console.WriteLine("1. UC1 - Validate First Name");
+           
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    FirstNamePattern.ValidateFirstName();
+                    break;
+                default:
+                    Console.WriteLine("Choose a appropriate option");
+                    break;
+            }
+        }
+    }
+}
